d52e9ca97397d5f83d43770ef75ec8900bec8862
albert.lawandos <albert.lawandos@epita.fr>
2022-02-11 17:32:58 +0100

    done

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,40 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Launcher {
-    public static void main (String args[]){
-        System.out.println("Welcome!");
-        Scanner sc = new Scanner(System.in);
-        String cmd = null;
-        
-        do {
-            System.out.println("Enter command: ");
-            cmd = sc.nextLine();
-            if ("quit".equals(cmd)){
-                break;
+    public static void main(String[] args) {
+        System.out.println("Bienvenue");
+        Scanner scanner = new Scanner(System.in);
+        List<Command> commands = new ArrayList<Command>();
+        commands.add(new Fibo());
+        commands.add(new Freq());
+        commands.add(new Quit());
+        commands.add(new Predict());
+        boolean isRunning = false;
+        while (!isRunning) {
+            System.out.println("Entrer commande :");
+            String input = scanner.nextLine();
+            Command command = commands.stream().filter(cmd -> input.equals(cmd.name())).findAny().orElse(null);
+
+            if (Objects.isNull(command)) {
+                System.out.println("Unknown command");
             }
-            if ("fibo".equals(cmd)){
-                System.out.println("Enter number: ");
-                int num = sc.nextInt();
-                sc.nextLine();
-                if (num == 0){
-                    System.out.println(0);
-                }
-                else if (num == 1){
-                    System.out.println(1);
-                }
-                else{
-                    int n1 = 0, n2 = 1, n3, i;
-                    for (i = 2; i <= num; ++i)
-                    {
-                        n3 = n1 + n2;
-                        n1 = n2;
-                        n2 = n3;
-                    }
-                    System.out.println("fibo: " + n2);
-                }
+            else {
+                isRunning = command.run(scanner);
             }
-            else System.out.println("Unkown command");
-        } while (true);
-        sc.close();
+        }
+        scanner.close();
     }
 }
---
+++ src/Predict.java
@@ -0,0 +1,68 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.StringJoiner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Predict implements Command{
+    @Override
+    public String name() {
+        return "predict";
+    }
+
+    @Override
+    public boolean run(Scanner scanner) {
+        System.out.println("Entrer le chemin du fichier");
+        String pathString = "";
+
+        try {
+            pathString = scanner.nextLine();
+            String text = Files.readString(Paths.get(pathString));
+            text = text.replaceAll("[.!?\\-,\\n]", " ").toLowerCase(Locale.ROOT);
+            List<String> tab = List.of(text.split(" "));
+
+            System.out.println("Entrer un mot");
+            String word = scanner.nextLine();
+            word = word.toLowerCase(Locale.ROOT);
+
+            if (!tab.contains(word)) {
+                System.out.println("Le mot n'est pas présent dans le fichier");
+                return false;
+            }
+
+            StringJoiner sentence = new StringJoiner(" ");
+            sentence.add(word);
+            for (int j = 0; j < 19; j++) {
+                List<String> next = new ArrayList<String>();
+                for (int i = 0; i < tab.size() - 1; i++) {
+                    if (tab.get(i).equals(word)) {
+                        next.add(tab.get(i + 1));
+                    }
+                }
+
+                Map<String, Long> next_list = next.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+                Map<String, Long> result = next_list.entrySet().stream()
+                        .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                        .limit(1)
+                        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+                List<String> list3 = new ArrayList<>(result.keySet());
+                String str = list3.get(0);
+                sentence.add(str);
+                word = str;
+            }
+            System.out.println(sentence);
+        } catch (IOException e) {
+            System.out.println("Unreadable file: " + e.getClass() + " " + e.getMessage());
+            return false;
+        }
+        return true;
+    }
+}
