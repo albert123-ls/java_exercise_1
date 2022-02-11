e723d3fadf8e66e3463059a3a215af1ebdd8478b
albert.lawandos <albert.lawandos@epita.fr>
2022-02-11 10:23:44 +0100

    exo4

--- src/Launcher.java
+++ src/Launcher.java
@@ -12,6 +12,27 @@ public class Launcher {
             if ("quit".equals(cmd)){
                 break;
             }
+            if ("fibo".equals(cmd)){
+                System.out.println("Enter number: ");
+                int num = sc.nextInt();
+                sc.nextLine();
+                if (num == 0){
+                    System.out.println(0);
+                }
+                else if (num == 1){
+                    System.out.println(1);
+                }
+                else{
+                    int n1 = 0, n2 = 1, n3, i;
+                    for (i = 2; i <= num; ++i)
+                    {
+                        n3 = n1 + n2;
+                        n1 = n2;
+                        n2 = n3;
+                    }
+                    System.out.println("fibo: " + n2);
+                }
+            }
             else System.out.println("Unkown command");
         } while (true);
     }
